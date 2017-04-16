4aa01bbc25aee1519337063f17185d8f31587626
twilabing <twilabing@gmail.com>
2017-04-15 20:00:52 -0700

    added birthdaycard

---
+++ birthdaycard/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace birthdaycard.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult CardForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CardForm(Models.cardInfo cardInfo)
+        {
+
+            if (ModelState.IsValid)
+            {
+                return View("Card", cardInfo);
+            }
+            else {
+                return View();
+            }
+            
+        }
+
+    }
+}
---
+++ birthdaycard/Models/cardInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace birthdaycard.Models
+{
+    public class cardInfo
+    {
+        [Required(ErrorMessage = "Please whom this card should be sent to")]
+        public string to { get; set; }
+
+        [Required(ErrorMessage = "Please who this card is from")]
+        public string from { get; set; }
+
+        [Required(ErrorMessage = "Please specify message to include")]
+        public string message { get; set; }
+    }
+}
